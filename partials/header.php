123230fd2a027b671c96a3d680b0d5f650645963
BaptisteCMY <baptiste.comoy2501@gmail.com>
2023-10-31 16:15:58 +0100

    test

--- partials/header.php
+++ partials/header.php
@@ -4,15 +4,20 @@
     
     <div class="header">
         
-    <a href="index.php" class="lien" style="font-size: 1.5em;">The Power of memory</a>
+    <a href="index.php" class="lien" style="font-size: 1.5em; <?php if($_SESSION['currentPage'] == 'acceuil'){ echo 'color: gray;'; } ?>">The Power of memory</a>
     <div class="header-right">
     <a href="index.php" class="lien">Accueil</a>
-    <a href="games/memory/index.php" class="lien">Jeu</a>
-    <a href="games/memory/scores.php" class="lien">Scores</a>
-    <a href="login.php" class="lien">Connexion</a>
-    <a href="register.php" class="lien">Inscription</a>
-    <a href="myAccount.php" class="lien">Mon espace</a>
-    <a href="contact.php" class="lien">Nous contacter</a>
+    <a href="games/memory/index.php" class="lien" style="<?php if($_SESSION['currentPage'] == 'Jeu'){ echo 'color: #EC9224;'; } ?>">Jeu</a>
+    <a href="games/memory/scores.php" class="lien" style="<?php if($_SESSION['currentPage'] == 'scores'){ echo 'color: #EC9224;'; } ?>">Scores</a>
+    <a href="login.php" class="lien" style="<?php if($_SESSION['currentPage'] == 'connexion'){ echo 'color: #EC9224;'; } ?>">Connexion</a>
+    <a href="register.php" class="lien" style="<?php if($_SESSION['currentPage'] == 'inscription'){ echo 'color: #EC9224;'; } ?>">Inscription</a>
+    <a href="myAccount.php" class="lien" style="<?php if($_SESSION['currentPage'] == 'espace'){ echo 'color: #EC9224;'; } ?>">Mon espace</a>
+    <a href="contact.php" class="lien" style="<?php if($_SESSION['currentPage'] == 'contact'){ echo 'color: #EC9224;'; } ?>">Nous contacter</a>
+    <?php 
+    if (isset($_SESSION['user'])) {
+        echo '<a href="disconnect.php" class="lien">'.$_SESSION['user']['pseudo'].'</a>';
+    }
+    ?>
         </div>
         
       </div>
