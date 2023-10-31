108840618367f483d5f855e6e8a2e57fc88c8147
BaptisteCMY <baptiste.comoy2501@gmail.com>
2023-10-31 11:35:30 +0100

    test

--- partials/header.php
+++ partials/header.php
@@ -1,123 +1,20 @@
-<?php 
-$currentPage = $_SERVER['REQUEST_URI'];
 
-if ($currentPage == 'games/memory/index.php'):?>
-  <div class="under-header" id="top">        
-    <div class="header"> 
-      <a href="index.php" class="lien" style="font-size: 1.5em;">The Power of memory</a>
-      <div class="header-right">
-      <a href="index.php" class="lien">Accueil</a>
-      <a href="games/memory/index.php" class="lien" style="color:orange">Jeu</a>
-      <a href="games/memory/scores.php" class="lien">Scores</a>
-      <a href="login.php" class="lien">Connexion</a>
-      <a href="register.php" class="lien">Inscription</a>
-      <a href="myAccount.php" class="lien">Mon espace</a>
-      <a href="contact.php" class="lien">Nous contacter</a>
+<div class="under-header" id="top">        
+    
+    <div class="header">
+        
+    <a href="index.php" class="lien" style="font-size: 1.5em;">The Power of memory</a>
+    <div class="header-right">
+    <a href="index.php" class="lien">Accueil</a>
+    <a href="games/memory/index.php" class="lien">Jeu</a>
+    <a href="games/memory/scores.php" class="lien">Scores</a>
+    <a href="login.php" class="lien">Connexion</a>
+    <a href="register.php" class="lien">Inscription</a>
+    <a href="myAccount.php" class="lien">Mon espace</a>
+    <a href="contact.php" class="lien">Nous contacter</a>
+        </div>
+        
       </div>
-    </div>
     </br></br></br>
-    <center><br><h1 style="margin-top: 0px; display: block;">The Power Of Memory</h1></center>
-  </div>
-  
-
-<?php elseif ($currentPage == 'games/memory/scores.php'):?> 
-  <div class="under-header" id="top">        
-    <div class="header"> 
-      <a href="index.php" class="lien" style="font-size: 1.5em;">The Power of memory</a>
-      <div class="header-right">
-      <a href="index.php" class="lien">Accueil</a>
-      <a href="games/memory/index.php" class="lien">Jeu</a>
-      <a href="games/memory/scores.php" class="lien">Scores</a>
-      <a href="login.php" class="lien">Connexion</a>
-      <a href="register.php" class="lien">Inscription</a>
-      <a href="myAccount.php" class="lien">Mon espace</a>
-      <a href="contact.php" class="lien">Nous contacter</a>
-      </div>
+      <center><br><h1 style="margin-top: 0px; display: block;">The Power Of Memory</h1></center>
     </div>
-    </br></br></br>
-    <center><br><h1 style="margin-top: 0px; display: block;">The Power Of Memory</h1></center>
-  </div>
-
-
-<?php elseif ($currentPage == 'MEMORIES/login.php'):?> 
-  <div class="under-header" id="top">        
-    <div class="header"> 
-      <a href="index.php" class="lien" style="font-size: 1.5em;">The Power of memory</a>
-      <div class="header-right">
-      <a href="index.php" class="lien">Accueil</a>
-      <a href="games/memory/index.php" class="lien">Jeu</a>
-      <a href="games/memory/scores.php" class="lien">Scores</a>
-      <a href="login.php" class="lien">Connexion</a>
-      <a href="register.php" class="lien">Inscription</a>
-      <a href="myAccount.php" class="lien">Mon espace</a>
-      <a href="contact.php" class="lien">Nous contacter</a>
-      </div>
-    </div>
-    </br></br></br>
-    <center><br><h1 style="margin-top: 0px; display: block;">The Power Of Memory</h1></center>
-  </div>
-
-<?php elseif ($currentPage == 'register.php'):?> 
-  <div class="under-header" id="top">        
-    <div class="header"> 
-      <a href="index.php" class="lien" style="font-size: 1.5em;">The Power of memory</a>
-      <div class="header-right">
-      <a href="index.php" class="lien">Accueil</a>
-      <a href="games/memory/index.php" class="lien">Jeu</a>
-      <a href="games/memory/scores.php" class="lien">Scores</a>
-      <a href="login.php" class="lien">Connexion</a>
-      <a href="register.php" class="lien">Inscription</a>
-      <a href="myAccount.php" class="lien">Mon espace</a>
-      <a href="contact.php" class="lien">Nous contacter</a>
-      </div>
-    </div>
-    </br></br></br>
-    <center><br><h1 style="margin-top: 0px; display: block;">The Power Of Memory</h1></center>
-  </div>
-
-<?php elseif ($currentPage == 'myAccount.php'):?> 
-  <div class="under-header" id="top">        
-    <div class="header"> 
-      <a href="index.php" class="lien" style="font-size: 1.5em;">The Power of memory</a>
-      <div class="header-right">
-      <a href="index.php" class="lien">Accueil</a>
-      <a href="games/memory/index.php" class="lien">Jeu</a>
-      <a href="games/memory/scores.php" class="lien">Scores</a>
-      <a href="login.php" class="lien">Connexion</a>
-      <a href="register.php" class="lien">Inscription</a>
-      <a href="myAccount.php" class="lien">Mon espace</a>
-      <a href="contact.php" class="lien">Nous contacter</a>
-      </div>
-    </div>
-    </br></br></br>
-    <center><br><h1 style="margin-top: 0px; display: block;">The Power Of Memory</h1></center>
-  </div>
-
-
-<?php elseif ($currentPage == 'contact.php'):?> 
-  <div class="under-header" id="top">        
-    <div class="header"> 
-      <a href="index.php" class="lien" style="font-size: 1.5em;">The Power of memory</a>
-      <div class="header-right">
-      <a href="index.php" class="lien">Accueil</a>
-      <a href="games/memory/index.php" class="lien">Jeu</a>
-      <a href="games/memory/scores.php" class="lien">Scores</a>
-      <a href="login.php" class="lien">Connexion</a>
-      <a href="register.php" class="lien">Inscription</a>
-      <a href="myAccount.php" class="lien">Mon espace</a>
-      <a href="contact.php" class="lien">Nous contacter</a>
-      </div>
-    </div>
-    </br></br></br>
-    <center><br><h1 style="margin-top: 0px; display: block;">The Power Of Memory</h1></center>
-  </div>
-
-
-<?php endif; ?>
-
-
-
-
-
-
-
